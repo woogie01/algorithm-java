011adaea8fb01a73029a53170b9cbef31be9b9b0
Hyukwon <113490741+woogie01@users.noreply.github.com>
2024-03-28 15:05:56 +0900

    comment 설명 추가

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -1,6 +1,10 @@
 class Solution {
     public long[] solution(int x, int n) {
+        
+        // 배열 크기 먼저 지정
         long[] answer = new long[n];
+
+        // 개별로 세팅
         long temp = 0;
         for (int i = 0; i < n; i++) {
             temp += x;
