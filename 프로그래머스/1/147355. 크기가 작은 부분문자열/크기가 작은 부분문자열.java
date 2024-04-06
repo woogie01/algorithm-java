404487a2d0d102e3dbf9b3ab2150698ce796e9eb
Hyukwon <113490741+woogie01@users.noreply.github.com>
2024-04-06 16:07:28 +0900

    Feat, 문제 해결 코드 - 권현욱

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -9,7 +9,8 @@ class Solution {
         
         int answer = 0;
         for (int i = 0; i < tLen - pLen + 1; i++) {
-            
+
+            // substring(시작 인덱스, 제외 인덱스)
             Long tNum = Long.parseLong(t.substring(i, i + pLen));
             
             if (tNum <= pNum) {
