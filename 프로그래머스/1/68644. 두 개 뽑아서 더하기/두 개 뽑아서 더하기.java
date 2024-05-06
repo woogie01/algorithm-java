9cce14b6175469d1b4031b885ffc2c950a5356b2
Hyukwon <113490741+woogie01@users.noreply.github.com>
2024-05-06 22:42:42 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.30 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,16 +1,22 @@
 import java.util.*;
 
 class Solution {
-    public Set<Integer> solution(int[] numbers) {
-        Set<Integer> answer = new TreeSet<>();
-        
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                int hap = numbers[i] + numbers[j];
-                answer.add(hap);
+    public int[] solution(int[] numbers) {
+        Set<Integer> haps = new TreeSet();
+
+        for(int i = 0; i < numbers.length - 1; i++){
+            for(int j = i + 1; j < numbers.length; j++){
+                haps.add(numbers[i] + numbers[j]);
             }
         }
-        
+
+        int[] answer = new int[haps.size()];
+        int index = 0;
+        Iterator itor = haps.iterator();
+        while(itor.hasNext()){
+            answer[index++] = (int)itor.next();
+        }
+
         return answer;
     }
 }
