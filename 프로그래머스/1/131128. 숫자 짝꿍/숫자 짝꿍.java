a5189f0f56e1254c6002b55a5d40825ce71e6ed4
Hyukwon <113490741+woogie01@users.noreply.github.com>
2024-05-25 20:54:33 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.24 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -2,39 +2,33 @@ import java.util.*;
 
 class Solution {
     public String solution(String X, String Y) {
+               
+        // 각 digit 개수
+        int[] xArr = new int[10];
+        int[] yArr = new int[10];
         
-        // <Digit, 개수>
-        Map<Integer, Integer> xMap = new HashMap<>();
-        Map<Integer, Integer> yMap = new HashMap<>();
-        
-        for (String str : X.split("")) {
-            int num = Integer.parseInt(str);
-            xMap.put(num, xMap.getOrDefault(num, 0) + 1);
+        for(String x : X.split("")){
+            int num = Integer.parseInt(x);
+            xArr[num]++;
         }
-        for (String str : Y.split("")) {
-            int num = Integer.parseInt(str);
-            yMap.put(num, yMap.getOrDefault(num, 0) + 1);
+        for(String y : Y.split("")){
+            int num = Integer.parseInt(y);
+            yArr[num]++;
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 9; i >= 0; i--) {
-            if (xMap.containsKey(i) && yMap.containsKey(i)) {
-                int cnt = Math.min(xMap.get(i), yMap.get(i));
-                for (int j = 0; j < cnt; j++) {
-                    sb.append(i);
-                }
+        for(int i = 9 ; i >= 0 ; i--){
+            
+            while(xArr[i]-- > 0 && yArr[i]-- > 0){
+                sb.append(i);
             }
         }
         
         String answer = sb.toString();
-        
-        // 예외 처리 : 0
-        if (answer.startsWith("0")) {
+        if(answer.startsWith("0")){
             return "0";
         }
-        
-        // 예외 처리 : 공백
-        if (answer.equals("")) {
+        if(answer.equals("")){
             return "-1";
         }
         
