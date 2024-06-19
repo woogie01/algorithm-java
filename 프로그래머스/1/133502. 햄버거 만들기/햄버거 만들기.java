ff72e739faa7312e6a97527279de391fa98e67ee
Hyukwon <113490741+woogie01@users.noreply.github.com>
2024-06-19 13:36:33 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -20,25 +20,22 @@ class Solution01 {
 
 class Solution02 {
     public int solution(int[] ingredients) {
-
+        
         int answer = 0;
         Stack<Integer> flow = new Stack<>();
         for (int cur : ingredients) {
-
-            flow.push(cur);
-
-            if(flow.size() >= 4) {
-
-                if (flow.get(flow.size() - 4) == 1
-                        && flow.get(flow.size() - 3) == 2
-                        && flow.get(flow.size() - 2) == 3
-                        && flow.get(flow.size() - 1) == 1) {
-                    answer++;
-                    for (int i = 0; i < 4; i++) {
-                        flow.pop();
-                    }
+            
+            flow.push(cur);    
+            if (flow.size() >= 4 
+                    && flow.get(flow.size() - 4) == 1
+                    && flow.get(flow.size() - 3) == 2
+                    && flow.get(flow.size() - 2) == 3
+                    && flow.get(flow.size() - 1) == 1) {
+                answer++;
+                for (int i = 0; i < 4; i++) {
+                    flow.pop();
                 }
-            }
+            }  
         }
         return answer;
     }
